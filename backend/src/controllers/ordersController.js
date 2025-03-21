0f01e929deb54554bba68c7593f382bc8770cd0c
G3RM4NT <gonzagerman924@gmail.com>
2025-03-21 08:02:48 -0600

    Orders finished brotherrrrrrrrrrrrrrrrrrrr

---
+++ backend/src/controllers/ordersController.js
@@ -0,0 +1,59 @@
+//Array de metodos (C R U D)
+const OrdersController = {};
+import OrdersModel from "../models/Orders";
+
+// SELECT
+OrdersController.getOrders = async (req, res) => {
+  const products = await OrdersModel.find();
+  res.json(products);
+};
+
+// INSERT
+OrdersController.createOrders = async (req, res) => {
+  const { IdCostumers,
+    products,
+    total,
+    status,
+     } = req.body;
+
+  const newOrders = new OrdersModel({ Nombre,
+    IdCostumers,
+    products,
+    total,
+    status,});
+  await newOrders.save();
+  res.json({ message: "orders saved" });
+};
+
+// DELETE
+OrdersController.deleteOrders = async (req, res) => {
+  const deleteorders = await OrdersModel.findByIdAndDelete(req.params.id);
+  if (!deleteorders) {
+    return res.status(404).json({ message: "orders not found" });
+  }
+  res.json({ message: "orders deleted" });
+};
+
+// UPDATE
+OrdersController.updateOrders = async (req, res) => {
+  // Solicito todos los valores
+  const {IdCostumers,
+    products,
+    total,
+    status, } = req.body;
+  // Actualizo
+  await OrdersModel.findByIdAndUpdate(
+    req.params.id,
+    {
+        IdCostumers,
+        products,
+        total,
+        status,
+    },
+    { new: true }
+  );
+  // muestro un mensaje que todo se actualizo
+  res.json({ message: "orders updated" });
+};
+
+export default OrdersController;
---
+++ backend/src/models/Orders.js
@@ -0,0 +1,87 @@
+/*
+Campos: 
+
+
+
+
+*/
+
+import { Schema, model} from "mongoose";
+
+const OrdersSchema = new Schema(
+{
+
+
+
+    idCostumers: {
+ 
+        type: Schema.Types.ObjectId,
+        ref: "costumers",
+        required: true,
+
+    },
+
+products: [
+
+{
+
+    idProduct: {
+ 
+        type: Schema.Types.ObjectId,
+        ref: "Products",
+        required: true,
+
+    },
+
+    quantity: {
+
+
+
+
+        type: Number,
+
+        min: [1, "La cantidad debe ser al menos 1"],
+        required: true,
+
+    },
+
+    subtotal: {
+
+
+        type: Number,
+        required: true,
+        min: [0, "El subtotal no puede ser negativo"],
+
+
+    },
+
+},
+
+
+
+],
+
+total: {
+
+    type: Number,
+    required: true,
+    min: [0, "El total no puede ser negativo"]
+},
+
+status: {
+
+    type: String,
+    required: true,
+   
+}
+
+
+},
+
+{
+    timestamps: true,
+    strict: false,
+}
+);
+
+export default model ("Products", OrdersSchema);

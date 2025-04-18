a508b4b572a9c899c769974d8663369da250c1ff
G3RM4NT <gonzagerman924@gmail.com>
2025-04-18 15:39:56 -0300

    Shoppingcar started

--- frontend/evo-peak/src/App.jsx
+++ frontend/evo-peak/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router";
 import Index from "./pages/index.jsx";
 import Navbar from './components/Navbar.jsx'
 import Wishlist from "./pages/wishlist.jsx"
+import Shoppingcar from "./pages/shoppingcar.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Index/>}/>
         <Route path="/wishlist" element={<Wishlist/>}/>
+        <Route path="/shoppingcar" element={<Shoppingcar/>}/>
       </Routes>
     </Router>
     </>

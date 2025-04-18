22a8b7c590fa4d056b7504ffce9907c32dfb9802
imanollramirez <aleimanolramirez@gmail.com>
2025-04-18 16:29:14 -0600

    Login Done

--- frontend/evo-peak/src/App.jsx
+++ frontend/evo-peak/src/App.jsx
@@ -1,33 +1,22 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from "react-router";
-import Index from "./pages/index.jsx";
-import Navbar from './components/Navbar.jsx';
-import Wishlist from "./pages/wishlist.jsx";
-import Login from "./pages/Login.jsx";
-=======
 import { BrowserRouter as Router, Routes, Route} from "react-router";
 import Index from "./pages/Index.jsx";
 import Navbar from './components/Navbar.jsx'
 import Wishlist from "./pages/wishlist.jsx"
+import Login from "./pages/Login.jsx"
 import Shoppingcar from "./pages/shoppingcar.jsx";
 import Products from "./pages/products.jsx";
->>>>>>> master
+
 
 function App() {  
     return (
     <Router>
      {window.location.pathname !== "/Login" && <Navbar/>}
       <Routes>
-<<<<<<< HEAD
-        <Route path="/" element={<Index />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/Login" element={<Login />} />
-=======
         <Route path="/" element={<Index/>}/>
+        <Route path="/Login" element={<Login/>}/>
         <Route path="/wishlist" element={<Wishlist/>}/>
         <Route path="/shoppingcar" element={<Shoppingcar/>}/>
         <Route path="/products" element={<Products/>}/>
->>>>>>> master
       </Routes>
     </Router>
   );

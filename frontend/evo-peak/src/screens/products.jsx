599398d4fe376333da304f2581ee9e8b13df51b4
dannyce27 <danielroland27@gmail.com>
2025-05-02 10:40:25 -0600

    Responsive in Products added

--- frontend/evo-peak/src/screens/products.jsx
+++ frontend/evo-peak/src/screens/products.jsx
@@ -1,33 +1,33 @@
-import rectangleDer from "../assets/Rectangle Der.png"
-import rectangleIzq from "../assets/Rectangle Izq.png"
-import FilterPill from "../components/filterPill.jsx"
-import "../screens/products.css"
+    import rectangleDer from "../assets/Rectangle Der.png"
+    import rectangleIzq from "../assets/Rectangle Izq.png"
+    import FilterPill from "../components/filterPill.jsx"
+    import "../screens/products.css"
 
-import ProductsCard from "../components/cardProducts.jsx";
+    import ProductsCard from "../components/cardProducts.jsx";
 
 
-const Products = () => {
-    return (
+    const Products = () => {
+        return (
 
-        <div className="products-container">
-            <h1 className="titulo">PRODUCTOS</h1>
-            <div className="filter-wrapper">
-                <FilterPill />
+            <div className="products-container">
+                <h1 className="titulo">PRODUCTOS</h1>
+                <div className="filter-wrapper">
+                    <FilterPill />
+                </div>
+                <img src={rectangleIzq} className="triangulo-izquierdo" alt="Triángulo Izquierdo" />
+                <img src={rectangleDer} className="triangulo-derecho" alt="Triángulo Derecho" />
+                <div className="center-content">
+                    <ProductsCard />
+                </div>
             </div>
-            <img src={rectangleIzq} className="triangulo-izquierdo" alt="Triángulo Izquierdo" />
-            <img src={rectangleDer} className="triangulo-derecho" alt="Triángulo Derecho" />
-            <div className="center-content">
-                <ProductsCard />
-            </div>
-        </div>
 
 
 
-    );
-};
+        );
+    };
 
 
 
 
 
-export default Products;
+    export default Products;

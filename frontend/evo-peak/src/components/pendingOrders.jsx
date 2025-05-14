24668156ae569c1360bdae3ff7d21ef4f7fba1a1
dannyce27 <danielroland27@gmail.com>
2025-05-13 22:52:12 -0600

    .

--- frontend/evo-peak/src/components/pendingOrders.jsx
+++ frontend/evo-peak/src/components/pendingOrders.jsx
@@ -1,9 +1,15 @@
 import "./pendingOrders.css";
+import TrianguloIzq from "../assets/Rectangle 474.png"
+import TrianguloDer from "../assets/Rectangle 475.png"
+import trianguloMedioIzq from "../assets/Group 10496.png"
+import trianguloMedioDer from "../assets/Group 10497.png"
 
 const pendingOrders = () => {
     return (
         <>
         <div className="pendingOrders"></div>
+
+
         </>
     );
 }

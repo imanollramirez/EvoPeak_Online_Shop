2c45c054277be9b26f061a358344de0cbf896725
dannyce27 <danielroland27@gmail.com>
2025-05-15 08:18:32 -0600

    Cards added

--- frontend/evo-peak/src/screens/private/OrdersPending.jsx
+++ frontend/evo-peak/src/screens/private/OrdersPending.jsx
@@ -1,10 +1,23 @@
+import PendingOrders from "../../components/Orders.jsx"
+import "../../screens/private/OrdersPending.css"
+
 const OrdersPending = () => {
 
 
     return (
 
         <>
-        <h1 className="titulo">Pedidos</h1>
+
+        <div className="main-container-ordersPending">
+            <h1 className="titulo-orderspending">Pedidos</h1>
+            <div className="container-cards">
+                <PendingOrders/>
+            </div>
+        </div>
+
+
         </>
     );
 };
+
+export default OrdersPending;

19def2beeb47db23beec9d6f7cd0327e9884d835
imanollramirez <aleimanolramirez@gmail.com>
2025-06-18 21:27:43 -0600

    working

--- frontend/evo-peak/src/components/NavbarAdmin.jsx
+++ frontend/evo-peak/src/components/NavbarAdmin.jsx
@@ -4,63 +4,64 @@ import "./Navbar.css"
 
 const Navbar = () => {
 
-return(
+  return (
     <>
-    <nav className="navbar p-0 fixed-top">
-  <div className="container-fluid d-block mt-0 pt-0"> 
-    <div className="row justify-content-between align-items-center">
+      <nav className="navbar p-0 fixed-top">
+        <div className="container-fluid d-block mt-0 pt-0">
+          <div className="row justify-content-between align-items-center">
 
-      <div className="col-auto mt-0 pt-0 ps-5">
-        <a className="navbar-brand" href="/welcome"><img src={Logo_Black} alt="Logo_White" width="180"/></a>
-      </div>
+            <div className="col-auto mt-0 pt-0 ps-5">
+              <a className="navbar-brand" href="/welcome"><img src={Logo_Black} alt="Logo_White" width="180" /></a>
+            </div>
 
-      <div className="col-auto d-flex justify-content-center mt-0 p-0">
-      <ul className="navbar navbar-list text-dark">
-        <li >
-          {location.pathname === "/welcome"  ? <a className="link-dark" href="/welcome">Inicio</a> : <a className="link-page" href="/welcome">Inicio</a>}
-        </li>
+            <div className="col-auto d-flex justify-content-center mt-0 p-0">
+              <ul className="navbar navbar-list text-dark">
+                <li >
+                  {location.pathname === "/welcome" ? <a className="link-dark" href="/welcome">Inicio</a> : <a className="link-page" href="/welcome">Inicio</a>}
+                </li>
 
-        <li >
-        {location.pathname === "/productsAdmin" ? <a className="link-dark" href="/productsAdmin">Productos</a> : <a className="link-page" href="/productsAdmin">Productos</a>}
-        </li>
+                <li >
+                  {location.pathname === "/productsAdmin" ? <a className="link-dark" href="/productsAdmin">Productos</a> : <a className="link-page" href="/productsAdmin">Productos</a>}
+                </li>
 
-        <li >
-        {location.pathname === "/ordersPending" ? <a className="link-dark" href="/ordersPending">Pedidos</a> : <a className="link-page" href="/ordersPending">Pedidos</a>}
-          
-        </li>
+                <li >
+                  {location.pathname === "/ordersPending" ? <a className="link-dark" href="/ordersPending">Pedidos</a> : <a className="link-page" href="/ordersPending">Pedidos</a>}
+
+                </li>
 
 
-        <li >
-        {location.pathname === "/UserAdmin" ? <a className="link-dark" href="/UserAdmin ">Usuarios</a> : <a className="link-page" href="/UserAdmin">Usuarios</a>}
-          
-        </li>
+                <li >
+                  {location.pathname === "/UserAdmin" ? <a className="link-dark" href="/UserAdmin ">Usuarios</a> : <a className="link-page" href="/UserAdmin">Usuarios</a>}
 
-        <li >
-        {location.pathname === "/employeesAdmin" ? <a className="link-dark" href="/employeesAdmin">Empleados</a> : <a className="link-page" href="/employeesAdmin">Empleados</a>}
-          
-        </li>
+                </li>
 
-       
-      </ul>
-      </div>
+                <li >
+                  {location.pathname === "/employeesAdmin" ? <a className="link-dark" href="/employeesAdmin">Empleados</a> : <a className="link-page" href="/employeesAdmin">Empleados</a>}
 
-      <div className="col-auto d-flex align-items-center mt-0 pt-0 pe-5">
-      <div className="search-container">
-    <div className="search-box">
-    <i className="fa-solid fa-magnifying-glass"/>
-    <input type="text" placeholder="Buscar" />
-    </div>
-    <i className="fa-solid fa-moon"></i>
-    {/*Temporary: link to the index, then i'll be linked to the profile. - Simulating the Log Out*/}
-    <a href="/"><img src="https://img.lovepik.com/png/20231128/3d-illustration-avatar-profile-man-collection-guy-cheerful_716220_wh860.png" className="avatar" /></a>
-    </div>
-      </div>
+                </li>
 
-    </div>
-  </div>
-</nav>
+
+              </ul>
+            </div>
+
+            <div className="col-auto d-flex align-items-center mt-0 pt-0 pe-5">
+              <div className="search-container">
+                <div className="search-box">
+                  <i className="fa-solid fa-magnifying-glass" />
+                  <input type="text" placeholder="Buscar" />
+                </div>
+                <i className="fa-solid fa-moon"></i>
+                  <a href="#">
+                    <img src="https://img.lovepik.com/png/20231128/3d-illustration-avatar-profile-man-collection-guy-cheerful_716220_wh860.png" className="avatar" />
+                  </a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </nav>
     </>
-)
+  )
 };
 
 export default Navbar;
